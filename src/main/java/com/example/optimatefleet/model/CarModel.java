b58ab87097becc45630e565110c1a6ca0bfb1277
MadsLindersson <Mali0002@stud.kea.dk>
2024-11-27 14:34:18 +0100

    US05 working progress. css almost done, fixed a flaw with electric cars.

--- src/main/java/com/example/optimatefleet/model/CarModel.java
+++ src/main/java/com/example/optimatefleet/model/CarModel.java
@@ -13,6 +13,23 @@ public class CarModel {
     public Gear_type gear_type;
     public Fuel_type fuel_type;
 
+    @Override
+    public String toString() {
+        return "CarModel{" +
+                "car_model_name='" + car_model_name + '\'' +
+                ", average_rental_time_in_months=" + average_rental_time_in_months +
+                ", price_a_month=" + price_a_month +
+                ", engine_size=" + engine_size +
+                ", seat_count=" + seat_count +
+                ", door_count=" + door_count +
+                ", horsepower=" + horsepower +
+                ", make=" + make +
+                ", body_type=" + body_type +
+                ", gear_type=" + gear_type +
+                ", fuel_type=" + fuel_type +
+                '}';
+    }
+
     public CarModel(String car_model_name, int price_a_month, double engine_size, int seat_count, int door_count, int horsepower, Make make, Body_type body_type, Gear_type gear_type, Fuel_type fuel_type) {
         this.car_model_name = car_model_name;
         this.average_rental_time_in_months = 0;
